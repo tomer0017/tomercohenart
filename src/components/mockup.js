b69b2d328d0c8d128c1eb8365bca7645de8442fd
tomer0017 <todent5@gmail.com>
2024-07-01 12:51:20 +0300

    logo size

--- src/components/mockup.js
+++ src/components/mockup.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React,{useState} from "react";
 
     function Mockup({picWidth,mockupPic, animate, animateType, mockupSofa}) {
+
+      const [changeFrame,setChangeFrame]=useState(false)
+
       return (
       <div>
-        <div className="clicked_mockup_pic">
-        <img src={mockupPic} style={{width:picWidth}} className={`mockup_pic ${animate ? animateType === 'in' ? 'slide-in-blurred-top' : 'slide-out-bck-center' : ''}`} />
-        </div>
+        
+        <img onClick={value=>setChangeFrame(!changeFrame)} src={mockupPic} style={{width:picWidth}} className={changeFrame?`mockup_pic ${animate ? animateType === 'in' ? 'slide-in-blurred-top' : 'slide-out-bck-center' : ''}`:'clicked_mockup_pic'} />
+        
         <img src={mockupSofa} className={`mockup_sofa ${animate ? animateType === 'in' ? 'bounce-in-top' : 'slide-out-bck-center' : ''}`} />
         <img className="mockup_left_pot" src="http://www.uploads.co.il/uploads/images/327016387.png" />
         {/* <img className="mockup_floor_lamp" src="http://www.uploads.co.il/uploads/images/66243597.png" /> */}
